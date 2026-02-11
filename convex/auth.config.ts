0dfa0b32ff8057d03a508924cbac63076f614e06
Abdullah <abdullah.sgahmed@gmail.com>
2026-02-11 14:37:26 +0200

    Remove WorkOS env var references from auth config

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -2,28 +2,7 @@ import type { AuthConfig } from "convex/server";
 
 // TODO: Re-enable WorkOS authentication once properly configured
 // For now, auth is disabled to allow deployment without WorkOS env vars
-const clientId = process.env.WORKOS_CLIENT_ID;
 
-const config: AuthConfig = clientId
-  ? {
-      providers: [
-        {
-          type: "customJwt",
-          issuer: "https://api.workos.com/",
-          algorithm: "RS256",
-          applicationID: clientId,
-          jwks: `https://api.workos.com/sso/jwks/${clientId}`
-        },
-        {
-          type: "customJwt",
-          issuer: `https://api.workos.com/user_management/${clientId}`,
-          algorithm: "RS256",
-          jwks: `https://api.workos.com/sso/jwks/${clientId}`
-        }
-      ]
-    }
-  : {
-      providers: []
-    };
-
-export default config;
+export default {
+  providers: []
+} satisfies AuthConfig;
